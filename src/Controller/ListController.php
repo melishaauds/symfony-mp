373821da5bdac0635c1493d7dd0877e7d57d7345
melishaauds <69965705+melishaauds@users.noreply.github.com>
2020-08-28 06:18:43 +0800

    Primary files for the program
    Includes bin, config, migration, public, src and templates folders

---
+++ src/Controller/ListController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListController extends AbstractController
+{
+    /**
+     * @Route("/list", name="list")
+     */
+    public function index(Request $request)
+    {
+        return $this->render('list/index.html.twig');
+    }
+}
